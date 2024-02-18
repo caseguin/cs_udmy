da06261c026288f330066c9f506c32c0bc2c77d0
caseguin <c.seguin48@gmail.com>
2024-02-18 11:30:15 -0500

    Initial commit

---
+++ nombre_magique/nombre_magique/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static int DemanderNombre(int nombreMin, int nombreMax)
+        {
+            int nombreInt = nombreMin-1;
+
+            while ((nombreInt < nombreMin) || (nombreInt > nombreMax))
+            {
+                Console.Write($"Entrez un nombre entre {nombreMin} et {nombreMax}:");
+                string nombreStr = Console.ReadLine();
+                
+                try 
+                {
+                    nombreInt = int.Parse(nombreStr);
+                    if ((nombreInt < nombreMin) || (nombreInt > nombreMax))
+                    {
+                        Console.WriteLine($"Erreur : Le nombre doit être compri entre {nombreMin} et {nombreMax}");
+                        nombreInt = 0;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Erreur : Entrer un nombre valide");
+                }
+
+            }
+            return nombreInt;
+        }
+        
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            const int NOMBRE_MIN = 1;
+            const int NOMBRE_MAX = 10;
+            int NOMBRE_MAGIQUE = rand.Next(NOMBRE_MIN, NOMBRE_MAX + 1);
+            int number = NOMBRE_MIN -1;
+            int nombreVie = 4;
+            
+            Console.WriteLine(NOMBRE_MAGIQUE);
+
+            while (nombreVie > 0)
+            {
+                number = DemanderNombre(NOMBRE_MIN, NOMBRE_MAX);
+                nombreVie -= 1;
+                Console.WriteLine("Nb vie : " + nombreVie);
+
+
+                if (nombreVie == 0)
+                {
+                    Console.WriteLine("GAME OVER");
+                    break;
+                }
+                else if (number < NOMBRE_MAGIQUE)
+                {
+                    Console.WriteLine("Le nombre est plus grand");
+                    
+                }
+                else if (number > NOMBRE_MAGIQUE)
+                {
+                    Console.WriteLine("Le nombre est plus petit");
+                }
+                else
+                {
+                    Console.WriteLine("Bravo, vous avez trouvé le nombre magique!");
+                }
+            }
+
+
+
+        }
+    }
+}
---
+++ premierProgramme/Program.cs
@@ -0,0 +1,108 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+
+namespace premierProgramme // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+
+        static string PersonName(int numeroPersonne)
+        {
+            string personName = "";
+            while (personName == "")
+            {
+                Console.Write($"Persone {numeroPersonne}, quel est ton nom? : ");
+                personName = Console.ReadLine();
+                personName = personName.Trim();
+
+                if (personName == "")
+                {
+                    Console.WriteLine("Erreur : le nom ne doit pas être vide");
+                }
+            }
+            return personName;
+        }
+
+        static int PersonAge(string personName)
+        {
+
+            int personAgeInt = 0;
+            while (personAgeInt <= 0)   
+            {
+                try
+                {
+                    
+                    Console.Write($"{personName}, quel est on âge? ");
+                    string personAgeStr = Console.ReadLine();
+                    personAgeInt = int.Parse(personAgeStr);
+
+                    if (personAgeInt < 0)
+                    {
+                        Console.WriteLine("Erreur : L'age ne doit pas être inférieur à 0");
+                    }
+                    else if (personAgeInt == 0)
+                    {
+                        Console.WriteLine("Erreur : L'âge doit être supérieur à 0");
+                    }
+                    
+                    
+                }
+                catch
+                {
+                    Console.WriteLine("Erreur : L'âge doit être un chiffre");
+                }
+            }
+            return personAgeInt;
+        }
+
+        static void AfficherInfoPersonne(string personeName, int personAge)
+        {
+            if (personAge == 18)
+            {
+                Console.WriteLine($"{personeName} a {personAge}, vous ête tout juste majeur");
+            }
+            else if (personAge == 17)
+            {
+                Console.WriteLine($"{personeName} a {personAge} et est majeur");
+            }
+            else if ((personAge >= 12) && (personAge <18))
+            {
+               Console.WriteLine($"{personeName} a {personAge}, vous êtes Ado"); 
+            }
+            else if ((personAge == 12) || (personAge == 2))
+            {
+               Console.WriteLine($"{personeName} a {personAge}, vous êtes un bébé"); 
+            }
+            else if (personAge > 60)
+            {
+                Console.WriteLine($"{personeName} a {personAge}, vous êtes un sénior"); 
+            }
+            else if (personAge > 18)
+            {
+                Console.WriteLine($"{personeName} a {personAge}, vous êtes majeur");
+            }
+            else if (personAge < 10)
+            {
+               Console.WriteLine($"{personeName} a {personAge}, vous êtes un enfant"); 
+            }
+            else if (personAge < 18)
+            {
+                Console.WriteLine($"{personeName} a {personAge}, vous êtes un mineur"); 
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string personName1 = PersonName(1);
+            string personName2 = PersonName(2);
+
+            int personAge1 = PersonAge(personName1);
+            int personAge2 = PersonAge(personName2);
+
+            AfficherInfoPersonne(personName1, personAge1);
+            AfficherInfoPersonne(personName2, personAge2);
+        }
+    }
+}
