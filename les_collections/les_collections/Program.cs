86ba04bcc0142850498d29d0167095ad04bb8654
caseguin <c.seguin48@gmail.com>
2024-02-18 14:16:49 -0500

    début les collections

---
+++ les_collections/les_collections/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+
+
+        static void AfficherTableau(int[] tableau)
+        {
+            for (int i=0; i<tableau.Length; i++)
+            {
+                Console.WriteLine($"[{i}] {tableau[i]}");
+            }
+        }
+
+        static void Tableaux()
+        {
+            int[] t = {200, 40, 15, 8, 12};
+            AfficherTableau(t);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Tableaux();
+        }
+    }
+}
